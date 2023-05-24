60aee6e2de57e79951ca59f997152bc943cf854b
Aiden Fine <fineaiden@gmail.com>
2023-05-24 18:50:37 -0400

    commit commit commit bb

--- frontend/src/components/jobsTable/JobsTable.jsx
+++ frontend/src/components/jobsTable/JobsTable.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'jobNumber', headerName: 'Job Number', width: 50 },
+  { field: 'id', headerName: 'ID', width: 70 },
   { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
   {
@@ -23,15 +23,15 @@ const columns = [
 ];
 
 const rows = [
-  { jobNumber: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { jobNumber: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { jobNumber: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { jobNumber: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { jobNumber: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { jobNumber: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { jobNumber: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { jobNumber: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { jobNumber: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 export default function JobsTable() {
@@ -40,7 +40,6 @@ export default function JobsTable() {
       <DataGrid
         rows={rows}
         columns={columns}
-        // getRowId={() => row._id}
         initialState={{
           pagination: {
             paginationModel: { page: 0, pageSize: 5 },
