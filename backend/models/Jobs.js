221b69d74fc5964e24870ca0e32a1dd3a92322e1
Aiden Fine <fineaiden@gmail.com>
2023-05-24 10:42:07 -0400

    job schema added

--- backend/models/Jobs.js
+++ backend/models/Jobs.js
@@ -2,23 +2,64 @@ import mongoose from 'mongoose';
 
 const JobsSchema = new mongoose.Schema(
     {
-        Jobs:{
-            type: String,
+        jobNumber:{
+            type: Number,
             required:true,
-            min: 2,
-            max: 100,
+            unique: true,
         },
-        assignedTo:{
-            type: String,
+        comp:{
+            type: Boolean,
+        },
+        invoiceNumber:{
+            type: Number,
             required:true,
-            max: 50,
             unique: true
         },
-        address:{
+        management:{
             type: String,
-            required:true,
-            min: 5
         },
+        hoa:{
+            type: String
+        },
+        rec:{
+            type: String
+        },
+        address:{
+            city:{
+                type: String,
+                required: false,
+            },
+            street:{
+                type: String
+            }
+        },
+        description:{
+            type: String,
+        },
+        lockbox:{
+            type: String
+        },
+        estNumber: {
+            type: String
+        },
+        estMoney:{
+            type: String
+        },
+        phone:{
+            type: String
+        },
+        scheduledDate:{
+            // NOT SURE YET 
+            type: String // CHANGE THIS LATER 
+        },
+        assignedTo:{
+            type: [
+                {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'User',
+                }
+            ]
+        }
     },
     { timestamps: true }
 );
