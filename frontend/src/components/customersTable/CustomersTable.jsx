3a948f1f59680b3003dc2afb4fae6d3ea1a51021
Aiden Fine <fineaiden@gmail.com>
2023-06-20 19:22:49 -0400

    Customer Table styled

--- frontend/src/components/customersTable/CustomersTable.jsx
+++ frontend/src/components/customersTable/CustomersTable.jsx
@@ -1,75 +1,100 @@
-import * as React from 'react';
-import { Grid } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import { Card, Menu, MenuItem, IconButton } from '@mui/material';
+import React from 'react';
+import {
+  Grid,
+  Card,
+  Table,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  IconButton,
+  TablePagination,
+} from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
+import { useNavigate } from 'react-router-dom';
+import { customTableCell, customTableFooter, customTableHead } from './style';
 
 const columns = [
-  {
-    field: 'edit',
-    headerName: '',
-    width: 80,
-    sortable: false,
-    renderCell: (params) => {
-      const navigateToDetails = () => {
-        // Handle navigation to customer details based on params.row._id
-        console.log('Navigate to details:', params.row._id);
-      };
-
-      return (
-        <IconButton onClick={navigateToDetails}>
-          <EditIcon />
-        </IconButton>
-      );
-    },
-  },
   { field: 'name', headerName: 'Full name', width: 130 },
   { field: 'email', headerName: 'Email', width: 200 },
   { field: 'notes', headerName: 'Notes', width: 350 },
 ];
 
-const CustomersTable = ({ data }) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  // eslint-disable-next-line no-unused-vars
-  const [selectedRow, setSelectedRow] = React.useState(null);
+const MAX_CHAR_LIMIT = 50;
 
-  const handleCellContextMenu = (params, event) => {
-    event.preventDefault();
-    setAnchorEl(event.currentTarget);
-    setSelectedRow(params.row);
+const truncateText = (text, maxLen) => {
+  if (text.length > maxLen) {
+    return text.slice(0, maxLen) + '...';
+  }
+  return text;
+};
+
+const CustomersTable = ({ data }) => {
+  const navigate = useNavigate();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
   };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    setSelectedRow(null);
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const startIndex = page * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const displayedData = data.slice(startIndex, endIndex);
+
+  const handleDetailsClick = (id) => {
+    navigate(`/customers/details/${id}`);
   };
 
   return (
     <Grid container justifyContent="center" alignItems="center">
-      <Card sx={{ width: '80%', height: 'auto' }}>
-        <DataGrid
-          rows={data}
-          columns={columns}
-          getRowId={(row) => row._id}
-          onCellContextMenu={handleCellContextMenu}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10, 15, 20, 25, 30, 35]}
-          disableSelectionOnClick
-          disableColumnMenu
-          checkboxSelection={false}
-          sx={{
-            outline: 'none',
-          }}
+      <Card sx={{ width: '80%', height: 'auto', marginTop: '25px' }}>
+        <TableContainer>
+          <Table>
+            <TableHead sx={customTableHead}>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell sx={customTableCell} key={column.field}>
+                    {column.headerName}
+                  </TableCell>
+                ))}
+                <TableCell sx={customTableCell}></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {displayedData.map((row) => (
+                <TableRow key={row._id}>
+                  {columns.map((column) => (
+                    <TableCell key={column.field} sx={customTableCell}>
+                      {truncateText(row[column.field], MAX_CHAR_LIMIT)}
+                    </TableCell>
+                  ))}
+                  <TableCell sx={customTableCell}>
+                    <IconButton onClick={() => handleDetailsClick(row._id)}>
+                      <EditIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          sx={customTableFooter}
+          rowsPerPageOptions={[5, 10, 15]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
         />
-        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-          {/* Add menu items for the right-click menu */}
-          <MenuItem onClick={handleMenuClose}>Edit</MenuItem>
-          <MenuItem onClick={handleMenuClose}>Delete</MenuItem>
-        </Menu>
       </Card>
     </Grid>
   );
--- frontend/src/components/customersTable/style.js
+++ frontend/src/components/customersTable/style.js
@@ -2,3 +2,16 @@ export const override = {
   justifyContent: 'center',
   alignItems: 'center',
 };
+
+export const customTableHead = {
+  backgroundColor: '#F9FAFB',
+  borderRadius: '15px',
+};
+
+export const customTableCell = {
+  borderBottom: 'none',
+};
+
+export const customTableFooter = {
+  backgroundColor: '#F9FAFB',
+};
